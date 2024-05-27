5c4824fd01d31b7255988c3a3977e02ff1a3d46a
Ruby Mateo <fangprincess_2k3@yahoo.com>
2024-05-28 00:10:34 +0800

    add update contact, error handling, validation and update router

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,25 +1,85 @@
 import express from "express";
+// prettier-ignore
+import {listContacts, getContactById, removeContact, addContact, updateContact} from "../../models/contacts.js";
+import { contactValidation } from "../../validations/validation.js";
+import { httpError } from "../../helpers/httpError.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (_req, res, next) => {
+  try {
+    const result = await listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await getContactById(contactId);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (!result) {
+      throw httpError(404);
+    }
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    // Preventing lack of necessary data
+    const { error } = contactValidation.validate(req.body);
+    if (error) {
+      throw httpError(400, "missing required name field");
+    }
+
+    const result = await addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await removeContact(contactId);
+
+    if (!result) {
+      throw httpError(404);
+    }
+
+    res.json({
+      message: "Contact deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = contactValidation.validate(req.body);
+    if (error) {
+      throw httpError(400, "missing fields");
+    }
+
+    const { contactId } = req.params;
+    const result = await updateContact(contactId, req.body);
+
+    if (!result) {
+      throw httpError(404);
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export { router };
